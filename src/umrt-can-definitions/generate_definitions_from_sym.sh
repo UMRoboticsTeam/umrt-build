38c9bde360d919d097c920ea838de8d899fd729c
Noah Reeder <noahreederatc@gmail.com>
2026-06-30 01:04:27 -0500

    Added DBC export to umrt_can_definitions

--- src/umrt-can-definitions/generate_definitions_from_sym.sh
+++ src/umrt-can-definitions/generate_definitions_from_sym.sh
@@ -3,9 +3,15 @@
 SYMBOL_FILE="$1"
 OUTPUT_FILE_BASENAME="$2"
 OUTPUT_FOLDER="$3"
+TRANSLATION_SCRIPT_PATH="$4"
+DBC_OUTPUT_PATH="$5"
+
+echo "Reading from '$SYMBOL_FILE'"
 
 # Strip "/slot:" tags, cantools doesn't like it
 sed -E "s|/slot:[0-9]+||g" < "$SYMBOL_FILE" | \
 sed -E "s|IDMask=[0-9A-F]+h||g" > "$OUTPUT_FOLDER/.tmp.sym"
 
 python3 -m cantools generate_c_source --database-name "$OUTPUT_FILE_BASENAME" -o "${OUTPUT_FOLDER}" "${OUTPUT_FOLDER}/.tmp.sym"
+
+python3 "$TRANSLATION_SCRIPT_PATH" "${OUTPUT_FOLDER}/.tmp.sym" "${DBC_OUTPUT_PATH}"
